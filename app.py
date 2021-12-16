74b74792d3343abfaf6ed6f0f883c74fe39895b5
Christian Llansola <christian@proptex.com>
2021-12-16 23:47:55 +0100

    endpoints

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from zipfile import ZipFile
 import shutil
 from compress import *
-from os import getcwd
+from os import abort, getcwd
 
 PATH_FILE = "%s/Customs/Cars/" % (getcwd())
+DOWNLOAD_PATH = "%s/skins/" % (getcwd())
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='')
 
-@app.route('/',methods = ['POST'])
+@app.route('/', methods = ['POST'])
 def zipdir():
     # content_type = request.headers.get('Content-Type')
     clean_dir()
     jsonfile = request.json
     JSONFILE = hydrate(jsonfile)
     write_json(JSONFILE)
-    shutil.make_archive('Customs', 'zip', 'Customs')
+    shutil.make_archive('./skins/Customs', 'zip', 'Customs')
     response = {'message': 'success'}
     return jsonify(response), 200
+@app.route('/get-files/<path:path>',methods = ['GET','POST'])
+def get_files(path):
+
+    """Download a file."""
+    try:
+        return send_from_directory(DOWNLOAD_PATH, path, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
+
+@app.route('/list/',methods=['GET'])
+def listar():
+   return jsonify(list_dir())
+
+@app.route('/clear_all/',methods=['DELETE'])
+def deleteFiles():
+   deleteSkins()
+   return jsonify({'delete':'true'})
--- compress.py
+++ compress.py
@@ -1,8 +1,11 @@
+from flask import jsonify
 import json
 import os, shutil
 from os import getcwd
 
 PATH_FILE = "%s/Customs/Cars/" % (getcwd())
+SKINS_FOLDER = "%s/skins/" % (getcwd())
+URL = "https://flask-h-deploy.herokuapp.com/get-files/"
 
 def hydrate(jsonObj: dict):
   filename = "test.json"
@@ -30,5 +33,37 @@ def clean_dir():
               os.unlink(file_path)
           elif os.path.isdir(file_path):
               shutil.rmtree(file_path)
+      except Exception as e:
+          print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def list_dir():
+  count = 0;
+  data = {'total_files':0,'all_files': None}
+  files = []
+  href:str='href';
+  
+  for filename in os.listdir(SKINS_FOLDER):
+      file_path = os.path.join(SKINS_FOLDER, filename)
+      try:
+          if os.path.isfile(file_path) or os.path.islink(file_path):
+              print(file_path)
+              files.append({href:URL + filename})
+              # os.unlink(file_path)
+          elif os.path.isdir(file_path):
+              print('')
+              # shutil.rmtree(file_path)
+      except Exception as e:
+          print('Failed to delete %s. Reason: %s' % (file_path, e))
+  data['total_files'] = len(files)
+  data['all_files'] = files
+  return data
+def deleteSkins():
+  files = []
+  for filename in os.listdir(SKINS_FOLDER):
+      file_path = os.path.join(SKINS_FOLDER, filename)
+      try:
+          if os.path.isfile(file_path) or os.path.islink(file_path):
+            
+              os.unlink(file_path)
       except Exception as e:
           print('Failed to delete %s. Reason: %s' % (file_path, e))
