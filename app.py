a854542ae7d5c06c115179881e5758806ae2238d
Christian Llansola <christian@proptex.com>
2021-12-20 16:24:01 +0100

    Ignore files and prefix api

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__,static_url_path='')
 app.register_blueprint(cars_api)
 app.register_blueprint(apifile_api)
 app.register_blueprint(files_api)
+app.register_blueprint(health_api)
 CORS(app)
 
 cors = CORS(app,resources={
--- routes/routes.py
+++ routes/routes.py
@@ -4,19 +4,21 @@ from os import abort, getcwd
 from handlers.compress import *
 from models.model import carList, carNotFound
 
+API_PREFIX = '/api/v1'
 PATH_FILE = "%s/Customs/Cars/" % (getcwd())
 DOWNLOAD_PATH = "%s/skins/" % (getcwd())
 
 cars_api = Blueprint('cars_api', __name__)
 apifile_api = Blueprint('apifile', __name__)
 files_api = Blueprint('files_api', __name__)
+health_api = Blueprint('health_api', __name__)
 
 #CARS ENDPOINT
-@cars_api.route('/cars/',methods=['GET'])
+@cars_api.route(API_PREFIX +'/cars/',methods=['GET'])
 def listarCarsCompleted():    
     return jsonify(carList())
 
-@cars_api.route('/cars/<idCar>',methods=['GET'])
+@cars_api.route(API_PREFIX +'/cars/<idCar>',methods=['GET'])
 def listarCars(idCar):    
     for element in carList():
         if (str(element['id_car']) == str(idCar)):
@@ -25,12 +27,12 @@ def listarCars(idCar):
             return jsonify(carNotFound())
     return jsonify(carList())
 #FILES ENDPOINTS
-@apifile_api.route('/clear_all/',methods=['DELETE'])
+@apifile_api.route(API_PREFIX +'/clear_all/',methods=['DELETE'])
 def deleteFiles():
    deleteSkins()
    return jsonify({'delete':'true'})
 
-@files_api.route('/', methods = ['POST'])
+@files_api.route(API_PREFIX +'/', methods = ['POST'])
 def zipdir():
     clean_dir()
     jsonfile = request.json
@@ -39,7 +41,7 @@ def zipdir():
     shutil.make_archive('./skins/Customs-'+jsonfile["racer"].lower()+'-'+str(jsonfile["number"]), 'zip', 'Customs')
     response = {'message': 'success'}
     return jsonify(response), 200
-@files_api.route('/get-files/<path:path>',methods = ['GET','POST'])
+@files_api.route(API_PREFIX +'/get-files/<path:path>',methods = ['GET','POST'])
 def get_files(path):
 
     """Download a file."""
@@ -49,6 +51,11 @@ def get_files(path):
         abort(404,{"error":"File not found"})
       
 
-@files_api.route('/list/',methods=['GET'])
+@files_api.route(API_PREFIX +'/list/',methods=['GET'])
 def listar():
    return jsonify(list_dir())
+
+#ALIVE
+@health_api.route(API_PREFIX + '/health/',methods=['GET'])
+def isAlive():    
+    return jsonify({"isAlive":"true"})
